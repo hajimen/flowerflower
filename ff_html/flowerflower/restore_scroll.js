67673bf88fecba92a1dff0067433029bb798dbed
NAKAZATO Hajime <hajime@kaoriha.org>
2011-12-02 17:45:55 +0900

    new function: store/load last viewed separation id

--- ff_html/flowerflower/restore_scroll.js
+++ ff_html/flowerflower/restore_scroll.js
@@ -20,9 +20,18 @@
 		$.cookie('last_scroll_position' + location.href, "" + pos, {expires: 3650, path: '/'});
 	}
 
+	function GetLastViewedSeparationId() {
+		return $.cookie('last_viewed_separation' + location.href);
+	}
+
+	function SetLastViewedSeparationId(separationId) {
+		$.cookie('last_viewed_separation' + location.href, "" + separationId, {expires: 3650, path: '/'});
+	}
+
 	function Tick() {
 		SetLastScrollPosition(window.pageYOffset);
 		SetLastScrollHeight($(document).height());
+		var y = GetLastScrollPosition();
 	}
 	
 	function RemoveStyleElement() {
@@ -32,21 +41,49 @@
 		}
 	}
 	
-	function RestoreScrollPosition() {
-		setTimeout(function() {
-			if (GetLastScrollHeight() != null) {
-				var y = parseInt(GetLastScrollPosition());
-				var h = parseInt(GetLastScrollHeight());
-				if ($(document).height() != h) {
-					y = (y * $(document).height()) / h;
-				}
-				window.scrollTo( 0, y);
+	function RestoreScrollPositionImpl() {
+		if (GetLastScrollPosition() != null) {
+			var y = GetLastScrollPosition();
+			var h = GetLastScrollHeight();
+			if ($(document).height() != h) {
+				y = (y * $(document).height()) / h;
 			}
-			setTimeout(function() {
-				RemoveStyleElement();
-			}, 1 );
-		}, 1 );
-	};
+			window.scrollTo(0, y);
+		}
+
+		var s = document.getElementById("beforeRestoreScrollPosition");
+		if (s) {
+			s.disabled = true;
+		}
+
+		Tick();
+
+		$('#' + GetLastViewedSeparationId()).nextAll().each(function(index, el) {
+			el.style.display = "block";
+		});
+		var s = $(".separation");
+		SetLastViewedSeparationId(s[s.length - 1].id);
+		setInterval(Tick, 1000);
+	}
+
+	function WaitForLayoutFixed(f, h) {
+		setTimeout(function() {
+			var nh = $(document).height();
+			if (nh == h) {
+				f();
+			} else {
+				WaitForLayoutFixed(f, nh);
+			}
+		}, 100);
+	}
+
+	function RestoreScrollPosition() {
+		if (GetLastScrollPosition() == null || GetLastScrollPosition() == 0) {
+			RestoreScrollPositionImpl();
+		} else {
+			WaitForLayoutFixed(RestoreScrollPositionImpl, $(document).height());
+		}
+	}
 	
 	function ContentUpdated() {
 		if  (!navigator.cookieEnabled) {
@@ -54,8 +91,15 @@
 			return;
 		}
 
-		RestoreScrollPosition();
-		setInterval(Tick, 1000);
+		if (GetLastViewedSeparationId() != null) {
+			$('#' + GetLastViewedSeparationId()).nextAll().each(function(index, el) {
+				el.style.display = "none";
+			});
+		}
+
+		setTimeout(function() {
+			RestoreScrollPosition();
+		}, 100);
 	}
 
 	window.ff.CharacterNoteElement = '<a href="character_note.html">';
