aac6afc6aa8597f700bc1b3614ce8e47a2bca40b
NAKAZATO Hajime <hajime@kaoriha.org>
2011-12-02 09:53:55 +0900

    error logging

--- ff_dotnet/FFSite/Global.asax.cs
+++ ff_dotnet/FFSite/Global.asax.cs
@@ -11,6 +11,7 @@ namespace FFSite
 {
     public class Global : System.Web.HttpApplication
     {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         protected void Application_Start(object sender, EventArgs e)
         {
@@ -20,6 +21,13 @@ namespace FFSite
         protected void Application_End(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError().GetBaseException();
+            logger.Error("Error Caught in Application_Error. Error in: " + Request.Url.ToString(), ex);
+            Server.ClearError();
         }
     }
 }
