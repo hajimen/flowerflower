5cd28016580258d232e3090b2bd17320ac4e09b0
NAKAZATO Hajime <hajime@kaoriha.org>
2012-05-21 14:22:09 +0900

    debug

--- ff_android/release-sign.bat
+++
@@ -1,3 +0,0 @@
-set PATH=%PATH%;%JAVA_HOME%\bin
-jarsigner -verbose -keystore C:\Users\Yotsuya\.android\release.keystore bin\ff_android-release-unsigned.apk kouchabutton-2
-C:\Users\Yotsuya\bin\android-sdk-windows\tools\zipalign -v 4 bin\ff_android-release-unsigned.apk bin\ff_android-release.apk
