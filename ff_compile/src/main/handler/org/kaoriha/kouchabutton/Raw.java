38a5f6c6e8134c123d8e33db8860f22447b986e1
NAKAZATO Hajime <hajime@kaoriha.org>
2012-05-22 12:05:04 +0900

    source format changed

--- ff_compile/src/main/handler/org/kaoriha/kouchabutton/Raw.java
+++ ff_compile/src/main/handler/org/kaoriha/kouchabutton/Raw.java
@@ -3,12 +3,14 @@ package org.kaoriha.kouchabutton;
 import org.kaoriha.flowerflower.compile.SourceProcessor;
 import org.kaoriha.flowerflower.compile.document.ElementHandler;
 
-public class Root extends ElementHandler<Document> {
+public class Raw extends ElementHandler<Document> {
 
 	@Override
 	public void handle(SourceProcessor sp, Object parent, Object target) {
-		org.kaoriha.flowerflower._20111001.Root r = (org.kaoriha.flowerflower._20111001.Root) target;
-		sp.process(r.getPOrChapterOrSection(), r);
+		org.kaoriha.flowerflower._20111001.Raw raw = (org.kaoriha.flowerflower._20111001.Raw) target;
+		if (raw.getContent() != null) {
+			doc.getHtml().raw(raw.getContent());
+		}
 	}
 
 }
---
+++ ff_compile/src/main/handler/org/kaoriha/kouchabutton/Span.java
@@ -0,0 +1,22 @@
+package org.kaoriha.kouchabutton;
+
+import org.kaoriha.flowerflower.compile.SourceProcessor;
+import org.kaoriha.flowerflower.compile.document.ElementHandler;
+
+public class Span extends ElementHandler<Document> {
+
+	@Override
+	public void handle(SourceProcessor sp, Object parent, Object target) {
+		org.kaoriha.flowerflower._20111001.Span span = (org.kaoriha.flowerflower._20111001.Span) target;
+		doc.getHtml().span();
+		if (span.getClazz() != null) {
+			doc.getHtml().classAttr(span.getClazz());
+		}
+		if (span.getStyle() != null) {
+			doc.getHtml().style(span.getStyle());
+		}
+		sp.process(span.getContent(), span);
+		doc.getHtml().end();
+	}
+
+}
