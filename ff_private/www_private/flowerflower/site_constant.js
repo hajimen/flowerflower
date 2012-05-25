2f805886dfdd787cb663cfa2218a26e22f80d80e
NAKAZATO Hajime <hajime@kaoriha.org>
2012-05-25 15:58:20 +0900

    image resources

--- ff_private/www_private/flowerflower/site_constant.js
+++ ff_private/www_private/flowerflower/site_constant.js
@@ -1,3 +1,3 @@
-window.ff.Site = "http://kaoriha.org/kouchabutton/";
-window.ff.Title = "紅茶ボタン";
-window.ff.DefaultPushMessage = "紅茶ボタンの新しい回が配信されました。";
+window.ff.Site = "http://kaoriha.org/kanzenhitogata/";
+window.ff.Title = "完全人型";
+window.ff.DefaultPushMessage = "完全人型ンの新しい回が配信されました。";
