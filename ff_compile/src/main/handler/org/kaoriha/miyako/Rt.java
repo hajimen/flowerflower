e53f0dfca5f61a273cef71d7571cec39fa693d17
NAKAZATO Hajime <hajime@kaoriha.org>
2013-02-22 10:10:45 +0900

    rp element in ruby element

--- ff_compile/src/main/handler/org/kaoriha/miyako/Rt.java
+++ ff_compile/src/main/handler/org/kaoriha/miyako/Rt.java
@@ -3,6 +3,7 @@ package org.kaoriha.miyako;
 import org.kaoriha.flowerflower.compile.SourceProcessor;
 import org.kaoriha.flowerflower.compile.document.ElementHandler;
 
+import com.googlecode.jatl.Html;
 import com.googlecode.jatl.MarkupBuilder.TagClosingPolicy;
 
 public class Rt extends ElementHandler<Document> {
@@ -10,7 +11,10 @@ public class Rt extends ElementHandler<Document> {
 	@Override
 	public void handle(SourceProcessor sp, Object parent, Object target) {
 		org.kaoriha.flowerflower._20130216.Rt rt = (org.kaoriha.flowerflower._20130216.Rt) target;
-		doc.getHtml().start("rt", TagClosingPolicy.NORMAL).text(rt.getContent()).end();
+		Html html = doc.getHtml();
+		html.start("rp", TagClosingPolicy.NORMAL).text("（").end();
+		html.start("rt", TagClosingPolicy.NORMAL).text(rt.getContent()).end();
+		html.start("rp", TagClosingPolicy.NORMAL).text("）").end();
 	}
 
 }
