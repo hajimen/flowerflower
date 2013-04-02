2b97d4b144706e82ff2b3e057a1eb35ade09d6d2
NAKAZATO Hajime <hajime@kaoriha.org>
2013-04-02 19:59:25 +0900

    rounded rectangle

---
+++ ff_ios2/ff_ios2/RoundedLabel.m
@@ -0,0 +1,38 @@
+//
+//  RoundedLabel.m
+//  ff_ios2
+//
+//  Created by 岩田 健一 on 13/04/02.
+//  Copyright (c) 2013年 NAKAZATO Hajime. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+#import "RoundedLabel.h"
+
+@implementation RoundedLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self roundInit];
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+-(void)roundInit {
+    CALayer *layer = self.layer;
+    layer.cornerRadius = 6.0f;
+    layer.masksToBounds = YES;
+}
+
+@end
--- ff_ios2/ff_ios2/TitleCell.m
+++ ff_ios2/ff_ios2/TitleCell.m
@@ -8,8 +8,12 @@
 #import "TitleCell.h"
 #import "TitleCollectionViewLayoutAttributes.h"
 #import "UIImage+Resize.h"
+#import "RoundedLabel.h"
 
-#define LEFT_VIEW_WIDTH 50.0
+#define LEFT_VIEW_WIDTH 77.0
+#define IMAGE_WIDTH 64.0
+#define MARGIN_X 9
+#define MARGIN_Y 14
 #define RIGHT_VIEW_WIDTH 50.0
 
 @interface TitleCell ()
@@ -24,21 +28,21 @@
 
 - (id)initWithFrame:(CGRect)frame {
     if ((self = [super initWithFrame:frame])) {
-        CGFloat height = frame.size.height;
+        CGFloat height = frame.size.height - MARGIN_Y * 2;
         CGFloat width = frame.size.width;
 
-        _leftView = [[UIView alloc] initWithFrame: CGRectMake(0.0, 0.0, LEFT_VIEW_WIDTH, height)];
+        _leftView = [[UIView alloc] initWithFrame: CGRectMake(MARGIN_X, MARGIN_Y, LEFT_VIEW_WIDTH - MARGIN_X, height)];
         _leftView.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleHeight;
         _leftView.backgroundColor = [UIColor blueColor];
         
         UIImage *img = [UIImage imageNamed:@"test_image.png"];
-        UIImageView *imgView = [[UIImageView alloc] initWithImage: [img resizedImageToFitInSize:CGSizeMake(LEFT_VIEW_WIDTH, height) scaleIfSmaller:YES]];
+        UIImageView *imgView = [[UIImageView alloc] initWithImage: [img resizedImageToFitInSize:CGSizeMake(IMAGE_WIDTH, height) scaleIfSmaller:YES]];
         [_leftView addSubview: imgView];
         
-        _middleView = [[UIView alloc] initWithFrame: CGRectMake(LEFT_VIEW_WIDTH, 0.0, width - LEFT_VIEW_WIDTH - RIGHT_VIEW_WIDTH, height)];
+        _middleView = [[UIView alloc] initWithFrame: CGRectMake(LEFT_VIEW_WIDTH, MARGIN_Y, width - LEFT_VIEW_WIDTH - RIGHT_VIEW_WIDTH - MARGIN_X * 2, height)];
         _middleView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 
-        _rightView = [[UIView alloc] initWithFrame: CGRectMake(width - RIGHT_VIEW_WIDTH, 0.0, RIGHT_VIEW_WIDTH, height)];
+        _rightView = [[UIView alloc] initWithFrame: CGRectMake(width - RIGHT_VIEW_WIDTH - MARGIN_X, MARGIN_Y, RIGHT_VIEW_WIDTH - MARGIN_X, height)];
         _rightView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleHeight;
         _rightView.backgroundColor = [UIColor yellowColor];
 
@@ -49,7 +53,28 @@
         label.textColor = [UIColor blackColor];
         label.text = @"test";
         
-        [_middleView addSubview: label];
+        UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(4.0, 0.0, _middleView.frame.size.width - 8.0, 40)];
+        titleLabel.autoresizingMask = UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth;
+        titleLabel.textAlignment = NSTextAlignmentLeft;
+        titleLabel.font = [UIFont boldSystemFontOfSize:18.0];
+        titleLabel.textColor = [UIColor blackColor];
+        titleLabel.numberOfLines = 0;
+        titleLabel.lineBreakMode = UILineBreakModeCharacterWrap;
+        titleLabel.text = @"東京特許許可局";
+        [_middleView addSubview: titleLabel];
+        
+        UILabel *tagLabel = [[RoundedLabel alloc] initWithFrame:CGRectMake(4.0, 44.0, _middleView.frame.size.width - 8.0, height)];
+        tagLabel.font = [UIFont systemFontOfSize:12.0];
+        tagLabel.textColor = [UIColor blackColor];
+        tagLabel.textAlignment = UITextAlignmentCenter;
+        tagLabel.text = @"TAG";
+        [tagLabel sizeToFit];
+        CGRect f = tagLabel.frame;
+        f.size.width += 12.0;
+        tagLabel.frame = f;
+        [_middleView addSubview: tagLabel];
+        
+        // [_middleView addSubview: label];
         [self.contentView addSubview: _leftView];
         [self.contentView addSubview: _middleView];
         [self.contentView addSubview: _rightView];
@@ -65,9 +90,9 @@
     TitleCollectionViewLayoutAttributes *attr = (TitleCollectionViewLayoutAttributes *) layoutAttributes;
 
     if (attr.row % 2 == 1) {
-        self.label.backgroundColor = [UIColor whiteColor];
+        self.contentView.backgroundColor = [UIColor whiteColor];
     } else {
-        self.label.backgroundColor = [UIColor redColor];
+        self.contentView.backgroundColor = [UIColor redColor];
     }
 }
 
